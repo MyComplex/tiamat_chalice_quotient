a0d4387c75951738dae6cff0502a87469a13e5fc
Jesus Santos <jo_nez@hotmail.com>
2023-05-20 11:56:04 -0700

    added questions and logic to display them with choice selection

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,67 @@
-var title = document.getElementById("showhide");
-var begin = document.getElementById("quiz_start");
+var title = document.getElementById("tts");
+var begin = document.getElementById("button");
+var question = document.getElementById("question");
+var answers_list = document.getElementById("choices");
+// var answer = do
+var timer = 5;
+var questions = [
+    {
+        "text": "Lorem",
+        "choice": [
+            "answer1",
+            "answer2",
+            "answer3",
+            "answer4"
+        ],
+        "correct": 4
+    },
+    {
+        "text": "ipsum",
+        "choices": [
+            "answer1",
+            "answer2",
+            "answer3",
+            "answer4"
+        ],
+        "correct": 1
+    },
+    {
+        "text": "dolor",
+        "choices": [
+            "answer1",
+            "answer2",
+            "answer3",
+            "answer4"
+        ],
+        "correct": 2
+    },
+    {
+        "text": "sit",
+        "choices": [
+            "answer1",
+            "answer2",
+            "answer3",
+            "answer4"
+        ],
+        "correct": 2
+    },
+    {
+        "text": "amet",
+        "choices": [
+            "answer1",
+            "answer2",
+            "answer3",
+            "answer4"
+        ],
+        "correct": 4
+    }
+];
+// var question = "";
 
 begin.addEventListener("click", function(){
-    title.style.display = "block";
+    title.textContent = timer;
+    question.textContent = questions[2].text;
+    answers_list.style.display = "flex";
+    // begin.style.display = "none";
     begin.textContent = "end";
 });
