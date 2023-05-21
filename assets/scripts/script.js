345fefaceac8a97a6f18a61209d4414048d25a58
Jesus Santos <jo_nez@hotmail.com>
2023-05-20 17:18:08 -0700

    working on question selection

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,30 @@
+/* variables */
+// change title, to timer, then score
 var title = document.getElementById("tts");
+// start and reset the quiz
 var begin = document.getElementById("button");
+// insert the question
 var question = document.getElementById("question");
+// insert the guesses
 var answers_list = document.getElementById("choices");
-// var answer = do
-var timer = 5;
+// state of quiz
+var quizStarted = false;
+// quiz timer
+var timer = 239;
+// timer interval
+var countdownInterval = setInterval(function () {
+    if (!quizStarted) {
+        return;
+    }
+    
+    timer--;
+    
+    if (timer <= 0) {
+        clearInterval(countdownInterval);
+        quizStarted = false;
+    }
+}, 1000);
+// question pool
 var questions = [
     {
         "text": "Lorem",
@@ -56,12 +77,35 @@ var questions = [
         "correct": 4
     }
 ];
+
+/* var ideas */
+// var answer = do
 // var question = "";
 
-begin.addEventListener("click", function(){
-    title.textContent = timer;
+/* init the quiz */
+begin.addEventListener("click", function () {
     question.textContent = questions[2].text;
     answers_list.style.display = "flex";
     // begin.style.display = "none";
     begin.textContent = "end";
+    quizStarted = true;
+    showCountdownTimer();
+    qselecter();
 });
+
+function qselecter() {
+for (var x=0;x<questions.length;x++)
+    console.log(questions[x]);
+}
+
+
+// function pageCounter() {
+//     var showQuestion = ;
+//     for (var x = 0; x < questions.length; x++) {
+
+//     }
+// }
+
+function showCountdownTimer() {
+    title.textContent = "Time left: " + timer;
+}
