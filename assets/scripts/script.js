c80852582a9885a55dc1d7611ab81e7e596e5b2b
Jesus Santos <jo_nez@hotmail.com>
2023-05-23 17:54:44 -0700

    did some consolidating

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,22 +1,16 @@
 /* VARIABLES */
-var title = document.getElementById("tts");
-var button = document.getElementById("btn");
-var question = document.getElementById("question");
-var choices = document.getElementById("choices");
-var quote = document.getElementById("quote");
-
-let questions = {},
+let title = document.getElementById("tts"),
+    button = document.getElementById("btn"),
+    questions = {},
     quotes = [],
     externalData = {},
     currentQuestion = 0,
     currentQuote = 0,
-    currentPage = 0,
+    currentPage = 1,
     selectedAnswers = [],
-    quizStarted = false,
     timer = 30,
     yourScore = 0,
     highScore = 0,
-    choicesLabels = ["l1", "l2", "l3", "l4"],
     choicesRadios = ["o1", "o2", "o3", "o4"];
 
 
@@ -27,59 +21,55 @@ fetch("./assets/json/data.json")
     }
     )
     .then(function (data) {
-        console.log(data);
         questions = data.questions;
         quotes = data.quotes;
         externalData = data;
     }
     );
 
-for (var dataItems in externalData) {
-    console.log(dataItems);
-    console.log(externalData[dataItems])
-}
-
 /* INIT THE QUIZ */
 button.addEventListener("click", function () {
-    if (button.textContent == "START") {
-        question.textContent = questions[currentQuestion].questionText;
-        questionsExchange();
-        choices.style.display = "block";
-        button.style["background-color"] = "purple";
-        button.textContent = "NEXT";
-        quote.textContent = quotes[currentQuote];
-        quizStarted = true;
-        currentPage = 1;
-        showCountdownTimer();
-        // timer interval
-        var countdownInterval = setInterval(function () {
+    let choices = document.getElementById("choices"),
+        quote = document.getElementById("quote"),
+        quizStarted = false,
+        countdownInterval = setInterval(function () {
             if (!quizStarted) {
                 return;
             }
             timer--;
-            if (currentPage == 5 || timer <= 0) {
-                clearInterval(countdownInterval);
-                quizStarted = false;
-            }
             showCountdownTimer();
         }, 1000);
-    }
-    /* CYCLE THROUGH QUESTIONS */
-    else if (currentPage < 5) {
 
-        currentQuestion = (currentQuestion + 1) % questions.length;
-        question.textContent = questions[currentQuestion].questionText;
+    if (button.textContent == "START") {
         questionsExchange();
-        currentQuote = (currentQuote + 1) % quotes.length;
-        quote.textContent = quotes[currentQuote];
-        currentPage++;
-        console.log(currentPage);
+        choices.style.display = "block";
+        button.style["background-color"] = "purple";
+        button.textContent = "NEXT";
+        quizStarted = true;
+        showCountdownTimer();
+        console.log("The If");
     }
+
+    /* CYCLE THROUGH QUESTIONS */
+    else if (currentPage < 6) {
+        questionsExchange();
+        console.log("The first Else If");
+    }
+
+    else if (currentPage == 6) {
+        button.textContent = "SUBMIT";
+        questionsExchange();
+        console.log("The second Else If");
+    }
+
     else {
-        console.log("End of quiz!");
         clearInterval(countdownInterval);
+        console.log("End of quiz!");
     }
+
+    console.log("The current page number is: " + currentPage);
 }
+
 );
 
 /*  FUNCTIONS */
@@ -87,21 +77,26 @@ function showCountdownTimer() {
     title.textContent = "You have " + timer + " seconds left.";
 };
 
-function nextQuestion() {
-    if (x >= questions.length - 1) {
-        return;
-    }
-    x++;
-};
-
 function questionsExchange() {
+    let choicesLabels = ["l1", "l2", "l3", "l4"],
+        question = document.getElementById("question");
     for (var x = 0; x < choicesLabels.length; x++) {
+        question.textContent = questions[currentQuestion].questionText;
         document.getElementById(choicesLabels[x]).textContent = questions[currentQuestion].choices[x];
-    }
+    };
+    currentQuestion = (currentQuestion + 1) % questions.length;
+    currentQuote = (currentQuote + 1) % quotes.length;
+    quote.textContent = quotes[currentQuote];
+    currentPage++;
 };
 
-function clearSelected(GroupName) {
-    var ele = document.getElementsByName(GroupName);
-    for (var i = 0; i < ele.length; i++)
-        ele[i].checked = false;
+function clearSelected() {
+    let radioGroupName = document.getElementsByName("choiceOptions")
+    for (let i = 0; i < radioGroupName.length; i++)
+        radioGroupName[i].checked = false;
+};
+
+/* CHECKS */
+this.onload = (event) => {
+    console.log("The current page number is: " + currentPage);
 };
