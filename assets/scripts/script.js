83a5b9507a9695391f5a00d7baf10cc72d1edde2
Jesus Santos <jo_nez@hotmail.com>
2023-05-26 07:43:09 -0700

    Revert "not sure what I did. taking a break"
    This reverts commit 4e79786bbecc1eba2d9153bb3d125eb60de9422d.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,8 +10,7 @@ let title = document.getElementById("tts"),
     timer = 30,
     yourScore = 0,
     highScore = 0,
-    choicesRadios = ["o1", "o2", "o3", "o4"],
-    // countdownInterval;
+    choicesRadios = ["o1", "o2", "o3", "o4"];
 
 
     /* FETCH QUESTIONS FROM JSON FILE */
@@ -25,19 +24,17 @@ let title = document.getElementById("tts"),
         });
 
 /* INIT THE QUIZ */
-button.addEventListener("click", function (event) {
-    console.log(event.target);
-    let choices = document.getElementById("choices");
-    countDown();
-    // quote = document.getElementById("quote"),
-    // quizStarted = false,
-    // countdownInterval = setInterval(function () {
-    //     if (!quizStarted) {
-    //         return;
-    //     }
-    //     timer--;
-    //     showCountdownTimer();
-    // }, 1000);
+button.addEventListener("click", function () {
+    let choices = document.getElementById("choices"),
+        quote = document.getElementById("quote"),
+        quizStarted = false,
+        countdownInterval = setInterval(function () {
+            if (!quizStarted) {
+                return;
+            }
+            timer--;
+            showCountdownTimer();
+        }, 1000);
 
     if (button.textContent == "START") {
         questionsExchange();
@@ -50,20 +47,19 @@ button.addEventListener("click", function (event) {
     }
 
     /* CYCLE THROUGH QUESTIONS */
-    else if (currentPage <= 4) {
+    else if (currentPage < 6) {
         questionsExchange();
         console.log("The first Else If");
     }
 
-    else if (currentPage == 5) {
+    else if (currentPage == 6) {
         button.textContent = "SUBMIT";
         questionsExchange();
         console.log("The second Else If");
     }
 
-    else if (currentPage >= 6) {
+    else {
         clearInterval(countdownInterval);
-        console.log(countdownInterval);
         console.log("End of quiz!");
     }
 
@@ -71,6 +67,10 @@ button.addEventListener("click", function (event) {
 });
 
 /*  FUNCTIONS */
+function showCountdownTimer() {
+    title.textContent = "You have " + timer + " seconds left.";
+};
+
 function questionsExchange() {
     let choicesLabels = ["l1", "l2", "l3", "l4"],
         question = document.getElementById("question");
@@ -88,43 +88,6 @@ function clearSelected() {
     let radioGroupName = document.getElementsByName("choiceOptions")
     for (let i = 0; i < radioGroupName.length; i++)
         radioGroupName[i].checked = false;
-};
-
-function getSelectedRadio() {
-    var radios = document.getElementsByName("choiceOptions");
-    var selected = Array.from(radios).find(radio => radio.checked);
-    selectedAnswers.push(selected.value);
-    console.log(selectedAnswers);
-};
-
-function tallyScore() {
-    let correctAnswers = ['4', '1', '2', '2', '3'];
-    for (let x = 0; x < questions.length; x++) {
-        if (correctAnswers[x] == questions[x].correctChoice) {
-            yourScore++;
-        }
-        return;
-    }
-};
-
-function countDown() {
-    timer--;
-    title.textContent = timer;
-
-    if (timer === 0) {
-        clearInterval(startTimer);
-    }
-};
-
-let startTimer = setInterval(countDown, 1000)
-
-function showCountdownTimer() {
-    timer--;
-    title.textContent = "You have " + timer + " seconds left.";
-    if (timer === 0 || currentPage >= 7) {
-        // title.textContent = "Quiz over.";
-        return;
-    }
 };
 
 /* CHECKS */
