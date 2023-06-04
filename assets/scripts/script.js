93b15021a025de4b6ed9e917ab8cd474d4947850
Jesus Santos <jo_nez@hotmail.com>
2023-06-03 20:50:20 -0700

    challenge complete

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -94,9 +94,10 @@ button.addEventListener("click", function (event) {
         if (timer <= 0) {
             clearInterval(countdownTimer);
             timerDisplay.textContent = "Time's up!"
-        }
-        if (currentQuestion >= 5) {
-            clearInterval(countdownTimer);
+            options.style.display = "none";
+            question.style.display = "none";
+            backButton.style.display = "flex";
+            title.textContent = "Sorry, please try again."
         }
     }, 1000);
     showQuestionText();
@@ -118,11 +119,11 @@ buttons.forEach(button => {
     })
 });
 
+/* CYCLE QUESTIONS */
 var currentQuestion = 0;
 var enterInitials = document.getElementById("initials");
 var subbtn = document.getElementById("submitScore");
 
-/* CYCLE QUESTIONS */
 function showQuestionText() {
     for (let i = 0; i < buttons.length; i++) {
         quote.textContent = quotes[randomNumber()];
@@ -133,7 +134,6 @@ function showQuestionText() {
             question.textContent = `Submit your name to save it to the leaderboard.`;
             enterInitials.style.display = "block";
             subbtn.style.display = "block";
-            // btnlbl.textContent = "SUBMIT";
             quote.textContent = quotes[10];
             clearInterval(countdownTimer);
             return;
@@ -146,6 +146,7 @@ function showQuestionText() {
     currentQuestion += 1;
 };
 
+/* SAVE PLAYER SCORE */
 var yourName = document.getElementById('initials');
 
 subbtn.addEventListener('click', function (event) {
@@ -154,8 +155,12 @@ subbtn.addEventListener('click', function (event) {
 });
 
 function saveScore(event) {
-    var storeName = yourName.value;
-localStorage.setItem(storeName, yourScore);
+    var player = yourName.value;
+    var score = yourScore;
+    var newScore = { player, score }
+    playerScores.push(newScore);
+    localStorage.setItem("quizScores", JSON.stringify(playerScores));
+    location.reload();
 };
 
 quote.textContent = quotes[randomNumber()];
@@ -163,3 +168,57 @@ quote.textContent = quotes[randomNumber()];
 function randomNumber() {
     return Math.floor(Math.random() * 15);
 };
+
+/* HANDLE HIGH SCORES */
+const headers = ["Player", "Score", "Rank"];
+const playerScores = JSON.parse(localStorage.getItem("quizScores"));
+
+let showTopScores = document.getElementById('leaderboard');
+let target = document.getElementById('dyntbl')
+let backButton = document.getElementById('back');
+
+showTopScores.addEventListener('click', (event) => {
+    event.preventDefault();
+    buildTable();
+});
+
+function buildTable() {
+    question.style.display = "none";
+    button.style.display = "none";
+    backButton.style.display = "flex";
+    allScores = playerScores.sort((a, b) => b.score - a.score);
+    for (let i = 0; i < allScores.length; i++) {
+        allScores[i].newProperty = i + 1;
+    }
+    const topScores = allScores.slice(0, 5);
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
+
+    table.appendChild(headerRow);
+
+    topScores.forEach(plyrScor => {
+        let row = document.createElement('tr');
+
+        Object.values(plyrScor).forEach(text => {
+            let cell = document.createElement('td');
+            let textNode = document.createTextNode(text);
+            cell.appendChild(textNode);
+            row.appendChild(cell);
+        });
+        table.appendChild(row);
+    });
+
+    target.appendChild(table);
+};
+
+/* RETURN TO BEGINNING */
+backButton.addEventListener('click', (event => {
+    location.reload()
+}
+));
